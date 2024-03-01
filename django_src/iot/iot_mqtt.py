7eb53b5eef72268471725f833ffe3c3844ee55f6
OctSakura <122379880+OctSakura@users.noreply.github.com>
2024-03-01 11:41:46 +0800

    update django

--- django_src/iot/iot_mqtt.py
+++ django_src/iot/iot_mqtt.py
@@ -1,20 +1,22 @@
 import paho.mqtt.client as mqtt
 from .models import Event
 import json
-ID="B05" # Sensor ID
+# ID="B05" # Sensor ID
 mqtt_broker = "broker.hivemq.com" # Broker
 mqtt_port = 1883 # Default
 mqtt_qos = 1 # Quality of Service = 1
-mqtt_topic = "iot/sensor-B05"
+mqtt_topic = "iot/sensor-AB"
 
 def mqtt_on_message(client, userdata, msg):
 # Do something
     d_msg = str(msg.payload.decode("utf-8"))
     iotData = json.loads(d_msg)
-    if iotData["node_id"] == ID:
-        print("Received message on topic %s : %s" % (msg.topic, iotData))
-        p = Event(node_id=iotData["node_id"], loc=iotData["loc"], temp=iotData["temp"] ,hum = iotData["hum"], light = iotData["light"], snd = iotData["snd"])
-        p.save()
+
+    #if iotData["node_id"] == ID:
+    
+    print("Received message on topic %s : %s" % (msg.topic, iotData))
+    p = Event(node_id=iotData["node_id"], loc=iotData["loc"], temp=iotData["temp"] ,hum = iotData["hum"], light = iotData["light"], snd = iotData["snd"])
+    p.save()
 
 mqtt_client = mqtt.Client("django-B05") # Create a Client Instance
 mqtt_client.on_message = mqtt_on_message
