214038012ac17fbc6a6a3f6a9164a1830fcb978f
OctSakura <woozie.wzt@gmail.com>
2024-04-16 12:56:02 +0800

    update iot_database

--- django_src/iot/admin.py
+++ django_src/iot/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Event
+from .models import tm1118_log
 
 admin.site.register(Event)
+admin.site.register(tm1118_log)
 # Register your models here.
--- django_src/iot/models.py
+++ django_src/iot/models.py
@@ -14,3 +14,17 @@ class Event(models.Model):
     #Methods
     def __str__(self):
         return 'Event #{}'.format(self.id)
+    
+class tm1118_log(models.Model):
+    #Fields
+    node_id = models.TextField()
+    loc = models.TextField()
+    temp = models.IntegerField()
+    hum = models.IntegerField()
+    light = models.IntegerField()
+    snd = models.IntegerField()
+    date_created = models.TextField()
+
+    #Methods
+    def __str__(self):
+        return 'tm1118_log #{}'.format(self.id)
