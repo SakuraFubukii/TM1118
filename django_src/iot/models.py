b7a5a2816d5311ac9954ce3fcd317cc35676001b
OctSakura <122379880+OctSakura@users.noreply.github.com>
2024-04-16 15:32:48 +0800

    update iot

--- django_src/iot/models.py
+++ django_src/iot/models.py
@@ -14,20 +14,6 @@ class Event(models.Model):
     #Methods
     def __str__(self):
         return 'Event #{}'.format(self.id)
-    
-class tm1118_log(models.Model):
-    #Fields
-    node_id = models.TextField()
-    loc = models.TextField()
-    temp = models.IntegerField()
-    hum = models.IntegerField()
-    light = models.IntegerField()
-    snd = models.IntegerField()
-    date_created = models.TextField()
-
-    #Methods
-    def __str__(self):
-        return 'tm1118_log #{}'.format(self.id)
 
 class Step(models.Model):
     value = models.DecimalField(max_digits=6, decimal_places=2)
